b4c737ec23f6411bb99703053ca9282b28707688
Arthur <arturs.kalnins@hotmail.com>
2019-02-20 20:43:33 +0200

    Homework3

---
+++ src/lv/Homework3/Human.java
@@ -0,0 +1,28 @@
+package lv.Homework3;
+
+public class Human {
+    private String name;
+    private int age;
+
+    public void sayHi() {
+
+        System.out.println("Hi! my name is " + getName() +  ", I'm " + getAge() + " years old!" );
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
---
+++ src/lv/Homework3/HumanSaysHi.java
@@ -0,0 +1,17 @@
+package lv.Homework3;
+
+public class HumanSaysHi {
+    public static void main(String[] args) {
+
+        Human myHuman = new Human();
+
+
+        myHuman.setName("Bobby");
+        myHuman.setAge(69);
+
+        myHuman.sayHi();
+
+    }
+
+
+}
