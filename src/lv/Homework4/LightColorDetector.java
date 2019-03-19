bdf39c1d3a84c80fc3d989f04e3890626d6c8796
Arthur <arturs.kalnins@hotmail.com>
2019-03-19 21:20:23 +0200

    HomeWork4

--- src/lv/Homework4/LightColorDetector.java
+++ src/lv/Homework4/LightColorDetector.java
@@ -1,5 +1,7 @@
 package lv.Homework4;
 
+import java.sql.SQLOutput;
+
 //OK
 public class LightColorDetector {
 
@@ -7,16 +9,22 @@ public class LightColorDetector {
         String color = "the light is not visible";
         if (waveLength >= 380 && waveLength < 450) {
             color = "Purple";
+            System.out.println("Purple");
         } else if (waveLength >= 450 && waveLength < 495) {
             color = "Blue";
+            System.out.println("Blue");
         } else if (waveLength >= 495 && waveLength < 570) {
             color = "Green";
+            System.out.println("Green");
         } else if (waveLength >= 570 && waveLength < 590) {
             color = "Yellow";
+            System.out.println("yellow");
         } else if (waveLength >= 590 && waveLength < 620) {
             color = "Orange";
+            System.out.println("Orange");
         } else if (waveLength >= 620 && waveLength < 750) {
             color = "Red";
+            System.out.println("Red");
         }
         return color;
     }
--- src/lv/Homework4/LightColorDetectorTest.java
+++ src/lv/Homework4/LightColorDetectorTest.java
@@ -2,8 +2,10 @@ package lv.Homework4;
 
 public class LightColorDetectorTest {
     public static void main(String[] args) {
+        LightColorDetector lcd = new LightColorDetector();
 
-        //Kur ir LightColorDetector izsaukums?
+        lcd.detect(399);
+
     }
 
 
