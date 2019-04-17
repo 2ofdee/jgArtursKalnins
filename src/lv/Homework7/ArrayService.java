db0b350bd18f098e990eb2ae6617a7c22efce4af
Arthur <arturs.kalnins@hotmail.com>
2019-04-17 21:58:06 +0300

    HomeWork7

---
+++ src/lv/Homework7/ArrayService.java
@@ -0,0 +1,49 @@
+package lv.Homework7;
+
+import java.util.Random;
+
+public class ArrayService {
+    public static void main(String[] args) {
+
+        create(3);
+        printArrayToConsole();
+        summInArray();
+        fillArrayWithRandomNumbers();
+    }
+
+    private static int[] anArray;
+
+    public static int[] create(int arraylenght) {
+        anArray = new int[arraylenght];
+        for (int i = 0; i < anArray.length; i++) {
+            anArray[i] = fillArrayWithRandomNumbers();
+        }
+        return anArray;
+    }
+
+    public static void printArrayToConsole() {
+        for (int n : anArray) {
+            System.out.println(n + " ");
+        }
+    }
+
+
+    public static int fillArrayWithRandomNumbers() {
+
+        Random rand = new Random();
+        int randomNum = rand.nextInt(1000);
+        return randomNum;
+
+    }
+
+    public static void summInArray() {
+        int sum = 0;
+        for (int i : anArray) {
+            sum += i;
+        }
+
+        System.out.println("sum of the an Array is " + sum);
+    }
+}
+
+
